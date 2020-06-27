a9eeea21386e1ca9b23a7165b1531633136b431c
Ross Baker <rossbaker32@gmail.com>
2020-06-27 17:37:41 +1000

    Add post and delete routes and actions

--- routes/api/items.js
+++ routes/api/items.js
@@ -20,6 +20,18 @@ router.post('/', (req, res) => {
   const newItem = new Item({
     name: req.body.name
   });
+  newItem.save()
+    .then(item => res.json(item))
+    .catch(error => console.log(error))
 }); 
+
+// @route DELETE api/items/:id
+// @desc  Delete An Item
+// @acess Public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(error => res.status(404).json({success: false}))
+})
 
 module.exports = router;
