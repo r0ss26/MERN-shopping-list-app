77f526992601716180f6ada08ab2ea98eefa5020
Ross Baker <rossbaker32@gmail.com>
2020-06-27 16:51:23 +1000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose'); // MongoDB ORM
+const bodyParser = require('body-parser'); // For parsing request body data
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(error => console.log(error))
+
+const port = process.env.PORT || 5000 // process.env.port is an environment variable for heroku deployment
+
+app.listen(port, () => console.log(`Server started on porrt ${port}`));
